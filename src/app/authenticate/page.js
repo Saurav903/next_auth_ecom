a0bb0f91a5437350234b0fe5f6211d8b47c0a7ae
saurav903 <sk8517167@gmail.com>
2023-09-19 16:32:19 +0530

    only login of auth0 added

---
+++ src/app/authenticate/page.js
@@ -0,0 +1,20 @@
+'use client';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Link from 'next/link';
+
+export default function Index() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  if (user) {
+    return (
+      <div>
+        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+      </div>
+    );
+  }
+
+  return <a href="/api/auth/login">Login</a>;
+}
